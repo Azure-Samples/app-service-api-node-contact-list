807dd27d99be111f3db0b225a7fcb3ab9c6eefab
Brady Gaster <bradyg@microsoft.com>
2015-11-23 10:43:09 -0800

    cleaning out

--- README.md
+++
@@ -1,1 +0,0 @@
-Node.js Contact List API App Sample
--- handlers/contacts.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
- var repository = require('../lib/contactRepository');
-
- module.exports = {
-     get: function contacts_get(req, res) {
-         res.json(repository.all())
-     }
- };
